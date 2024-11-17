39135398196aec9070c6c56836c2550c8ab2dbb5
huumanh1612 <21522331@gm.uit.edu.vn>
2024-11-17 18:38:33 +0700

    admin logo change

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import './Navbar.css'
 import {assets} from '../../assets/assets'
+import foodLogo from '../../assets/food-logo-template.png';  
 
 const Navbar = () => {
   return (
     <div className='navbar'>
-      <img className='logo' src={assets.logo} alt="" />
+      <img src={foodLogo} alt="Logo" className="logo" />
       <img className='profile' src={assets.profile_image} alt="" />
       
     </div>
